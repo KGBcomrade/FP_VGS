cfd46762e9904206a83559f29e7ba1985cb20555
KGBcomrade <vlasov.sviatoslav@gmail.com>
2020-04-29 22:38:18 +0300

    Added Layer struct

---
+++ Misc/Layer.h
@@ -0,0 +1,18 @@
+//
+// Created by kgbcomrade on 4/29/20.
+//
+
+#ifndef FP2_LAYER_H
+#define FP2_LAYER_H
+
+
+#include <vector>
+#include <SFML/Graphics/Sprite.hpp>
+
+struct Layer {
+    int opacity;
+    std::vector<sf::Sprite> tiles;
+};
+
+
+#endif //FP2_LAYER_H
